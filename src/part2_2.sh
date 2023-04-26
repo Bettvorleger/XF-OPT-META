e236b5487dec138d77aafac7f74c20c75000c078
Daniel Werner <daniel-werner96@web.de>
2023-04-26 15:31:10 +0200

    Cleanup for shell scripts

--- src/part2_2.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-python3.11 main.py -p pr226 -m opt -di 0.1 -opt gradient -oc 60 -i 6 &> output.log &
-python3.11 main.py -p pr226 -m opt -di 0.25 -opt gradient -oc 60 -i 6 &> output.log &
-python3.11 main.py -p pr226 -m opt -di 0.5 -opt gradient -oc 60 -i 6 &> output.log &
--- src/part2_3.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-python3.11 main.py -p pr136 -m opt -di 0.25 -opt gradient -oc 60 -i 6 &> output.log &
-python3.11 main.py -p pr226 -m opt -di 0.25 -opt gradient -oc 60 -i 6 &> output.log &
-python3.11 main.py -p d198 -m opt -di 0.25 -opt gradient -oc 60 -i 6 &> output.log &
-wait
-python3.11 main.py -p pr136 -m opt -di 0.5 -opt gradient -oc 60 -i 6 &> output.log &
-python3.11 main.py -p pr226 -m opt -di 0.5 -opt gradient -oc 60 -i 6 &> output.log &
-python3.11 main.py -p d198 -m opt -di 0.5 -opt gradient -oc 60 -i 6 &> output.log &
